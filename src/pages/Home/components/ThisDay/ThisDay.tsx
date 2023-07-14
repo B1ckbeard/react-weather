061c03629c743bb667afc0324a3ac0615881893c
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2023-07-14 10:58:37 +0300

    React-weather project

---
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './ThisDay.module.scss'
+
+type Props = {}
+
+const ThisDay = (props: Props) => {
+  return (
+    <header className={s.header}></header>
+  )
+}
+
+export default ThisDay;
